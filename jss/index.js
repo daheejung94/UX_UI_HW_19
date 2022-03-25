c7159cdaa93f23e7ee4eccde1dd832802c92353d
daheejung94 <daheejung94@gmail.com>
2022-03-24 20:50:41 -0700

    Push 1

---
+++ jss/index.js
@@ -0,0 +1,9 @@
+console.log("Your index.js file is loaded correctly");
+
+$("#myBtn").on("click", function() {
+  console.log("Please work");
+  $(".modal-content").css("display", "block");
+});
+
+
+
